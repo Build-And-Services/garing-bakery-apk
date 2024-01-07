ec7a6b3ff92556e67f535c4d1c074be1c4f143f4
Heri Setyawan <herisetyawan233@gmail.com>
2024-01-07 22:28:09 +0700

    feat: dashboard layout

--- lib/features/dashboard/presenter/pages/sub_page/home.dart
+++ lib/features/dashboard/presenter/pages/sub_page/home.dart
@@ -1,6 +1,8 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:garing_bakery_apk/core/config/theme.dart';
 import 'package:garing_bakery_apk/features/dashboard/presenter/widgets/drawer_widget.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeSubPage extends StatelessWidget {
   const HomeSubPage({super.key});
@@ -18,6 +20,192 @@ class HomeSubPage extends StatelessWidget {
         ),
         centerTitle: true,
         backgroundColor: MyTheme.primary,
+      ),
+      body: ListView(
+        // crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: 200,
+            width: double.infinity,
+            decoration: const BoxDecoration(
+              color: MyTheme.primary,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(30),
+                bottomRight: Radius.circular(30),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20.0, left: 20),
+            child: Text(
+              "Semua Kategori",
+              style: GoogleFonts.poppins(
+                  fontSize: 18, fontWeight: FontWeight.w500),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          CarouselSlider(
+            options: CarouselOptions(
+              height: 100,
+              viewportFraction: 0.3,
+              aspectRatio: 5.0,
+              initialPage: 0,
+            ),
+            items: const [
+              CategoryBox(),
+              CategoryBox(),
+              CategoryBox(),
+              // CategoryBox(),
+              // CategoryBox(),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20.0, left: 20),
+            child: Text(
+              "Semua Barang",
+              style: GoogleFonts.poppins(
+                  fontSize: 18, fontWeight: FontWeight.w500),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          // Expanded(
+          Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+            ),
+            child: GridView(
+              physics: const NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 0.75,
+              ),
+              children: const [
+                // Product Card
+                ProductItem(),
+                ProductItem(),
+                ProductItem(),
+                ProductItem(),
+              ],
+            ),
+          ),
+          // ),
+        ],
+      ),
+    );
+  }
+}
+
+class ProductItem extends StatelessWidget {
+  const ProductItem({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: const Color.fromARGB(255, 232, 232, 232),
+      shadowColor: Colors.black12,
+      borderOnForeground: true,
+      semanticContainer: true,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: double.infinity,
+            height: 150,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(
+                Radius.circular(10),
+              ),
+              image: DecorationImage(
+                image: AssetImage('assets/product.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Column(
+                        children: [
+                          Text(
+                            "Product 1",
+                            style: GoogleFonts.poppins(
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          const Text("Roti Basah"),
+                        ],
+                      ),
+                      Text(
+                        "200",
+                        style: GoogleFonts.poppins(
+                          fontSize: 12,
+                          color: const Color.fromARGB(255, 32, 138, 35),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  ),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("harga: "),
+                      Text("Rp. 10.000"),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class CategoryBox extends StatelessWidget {
+  const CategoryBox({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(
+        right: 20,
+      ),
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        image: const DecorationImage(
+          image: AssetImage('assets/product.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Center(
+        child: Text(
+          "Roti Basah",
+          style: GoogleFonts.poppins(
+            fontSize: 14,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
       ),
     );
   }
