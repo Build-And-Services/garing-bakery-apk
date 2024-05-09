f1d2331f8aff27dd7aecf8d8e45fba834ea43d9b
Heri Setyawan <herisetyawan233@gmail.com>
2024-05-09 17:47:06 +0700

    role admin

--- lib/features/dashboard/presenter/pages/dashboard_page.dart
+++ lib/features/dashboard/presenter/pages/dashboard_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:garing_bakery_apk/core/config/theme.dart';
 import 'package:garing_bakery_apk/core/routes/app.dart';
+import 'package:garing_bakery_apk/features/auth/presenter/provider/auth_provider.dart';
 import 'package:garing_bakery_apk/features/dashboard/presenter/provider/dashboard_provider.dart';
 import 'package:garing_bakery_apk/features/profile/presenter/pages/profile_page.dart';
 import 'package:garing_bakery_apk/features/transaction/presenter/pages/history/transactions.dart';
@@ -23,36 +24,82 @@ class DashboardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     DashboardProvider dashboardProvider = context.watch<DashboardProvider>();
-    return Scaffold(
-      body: _pages[dashboardProvider.selectedTab],
-      floatingActionButton: dashboardProvider.selectedTab == 0 ||
-              dashboardProvider.selectedTab == 2
-          ? FloatingActionButton(
-              backgroundColor: MyTheme.primary,
-              onPressed: () {
-                Navigator.of(context).pushNamed(Routes.TRANSACTIONS);
-              },
-              child: const Icon(
-                Icons.add,
-                color: Colors.white,
+    final authProvider = context.read<AuthProvider>();
+    String? role;
+    if (authProvider.userCache != null) {
+      role = authProvider.userCache!.role;
+    }
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (didPop) {
+        showDialog<bool>(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Are you sure?'),
+              content: const Text(
+                'Are you sure you want to leave this page?',
               ),
-            )
-          : null,
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: dashboardProvider.selectedTab,
-        onTap: (index) => dashboardProvider.changeTab = index,
-        selectedItemColor: MyTheme.primary,
-        unselectedItemColor: Colors.grey,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.dashboard), label: "Home"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.shopping_bag), label: "Produk"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.shopping_cart), label: "Transaksi"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.category), label: "Kategori"),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profiles"),
-        ],
+              actions: <Widget>[
+                TextButton(
+                  style: TextButton.styleFrom(
+                    textStyle: Theme.of(context).textTheme.labelLarge,
+                  ),
+                  child: const Text('Nevermind'),
+                  onPressed: () {
+                    Navigator.pop(context, false);
+                  },
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    textStyle: Theme.of(context).textTheme.labelLarge,
+                  ),
+                  child: const Text('Leave'),
+                  onPressed: () {
+                    Navigator.pop(context);
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            );
+          },
+        );
+        return;
+      },
+      child: Scaffold(
+        body: _pages[dashboardProvider.selectedTab],
+        floatingActionButton: role == "admin"
+            ? null
+            : dashboardProvider.selectedTab == 0 ||
+                    dashboardProvider.selectedTab == 2
+                ? FloatingActionButton(
+                    backgroundColor: MyTheme.primary,
+                    onPressed: () {
+                      Navigator.of(context).pushNamed(Routes.TRANSACTIONS);
+                    },
+                    child: const Icon(
+                      Icons.add,
+                      color: Colors.white,
+                    ),
+                  )
+                : null,
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: dashboardProvider.selectedTab,
+          onTap: (index) => dashboardProvider.changeTab = index,
+          selectedItemColor: MyTheme.primary,
+          unselectedItemColor: Colors.grey,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.dashboard), label: "Home"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.shopping_bag), label: "Produk"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.shopping_cart), label: "Transaksi"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.category), label: "Kategori"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person), label: "Profiles"),
+          ],
+        ),
       ),
     );
   }
